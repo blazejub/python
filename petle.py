c5b82adf97a6124ee33a406598037a42e05f540b
blazejub <90357483+blazejub@users.noreply.github.com>
2021-11-03 12:25:19 +0100

    Add files via upload

---
+++ petle.py
@@ -0,0 +1,18 @@
+"""for i in range(0,11,2):
+    print(i)
+"""
+"""for a in range(50,151,3):
+    print(a)
+"""
+"""
+tab = [1,2,51,52,51,63,64,67]
+min = None
+for i in tab:
+    if min == None or min >i:
+        min = i
+print("Najmniejasza to:",min)
+"""
+a = 0
+for i in range(0,11):
+    print (i + a)
+    a=i
